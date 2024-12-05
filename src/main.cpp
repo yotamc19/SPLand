bbb800ede9da76f58256d273abd05dac65d0f89d
Jotam Combe <jotamc@wix.com>
2024-12-05 15:39:46 +0000

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,7 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    FacilityType ft("School", FacilityCategory::LIFE_QUALITY, 1, 3, 1, 1);
-    Facility f(ft, "Beer Sheba");
-    FacilityStatus fs = f.getStatus();
-    if (fs == FacilityStatus::OPERATIONAL) {
-        cout << "operation" << endl;
-    } else {
-        cout << "under construction" << endl;
-    }
-
     string configurationFile = argv[1];
-    cout << configurationFile << endl;
     Simulation simulation(configurationFile);
     simulation.start();
     if (backup != nullptr) {
